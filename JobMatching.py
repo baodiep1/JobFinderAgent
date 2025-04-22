d86e88cd1c18e622a2b5c6dcf4eab6cbe5060dd2
Geshvad <geshvad@MacBook-Air.local>
2025-04-21 20:36:31 -0400

    removed data

--- JobMatching.py
+++ JobMatching.py
@@ -8,7 +8,7 @@ params = {
   "engine": "google",
   "q": "Python Software Engineer NLP remote",
   "location": "United States",
-  "api_key": "561af63ae039653cca273b9efa016b2bb44f5536499eb1b770eb1371df27f3f6"
+  "api_key": "************************************"
 }
 
 search = GoogleSearch(params)
