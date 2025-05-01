2ab0102e99d2f581e7af65c5f3ecd4b7b2979948
BBuns <168569587+baodiep1@users.noreply.github.com>
2025-05-01 13:24:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,10 @@ import os
 import re
 import spacy
 import nltk
-from pathlib import Path
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from tools.pdf_extractor import extract_text_from_pdf
 from tools.google_search import search_google_jobs
-
-# Load model from local folder
-MODEL_PATH = Path(__file__).parent / "models/en_core_web_sm/en_core_web_sm-3.8.0"
-nlp = spacy.load(str(MODEL_PATH))
 
 # Download necessary NLTK data
 try:
